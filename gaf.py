b6b2008848ee1c0e4f7bd2ca278b0b5025cf4bdd
Gustav <sir.gustav.the.coder@gmail.com>
2017-04-05 08:11:27 +0200

    added pep spaces

--- gaf.py
+++ gaf.py
@@ -182,12 +182,14 @@ def read_struct(f, tl):
 
     return struct
 
+
 class File:
     def __init__(self):
         self.structs = []
 
     def __str__(self):
         return '\n'.join([str(x) for x in self.structs])
+
 
 def read_several_structs(f):
     file = File()
