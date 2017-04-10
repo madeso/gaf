bc16917393122fd94b12829b133f6b6cf8ac3cc3
Gustav <sir.gustav.the.coder@gmail.com>
2017-04-10 07:54:47 +0200

    faulty c++ test code

--- test-cpp/onestruct.cc
+++ test-cpp/onestruct.cc
@@ -1,3 +1,17 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "mygaf.h"
+
+TEST_CASE("constructor") {
+  Foo foo;
+  REQUIRE(foo.GetHello() == 0);
+  REQUIRE(foo.GetWorld() == 0.0f);
+}
+
+TEST_CASE("constructor") {
+  Foo foo;
+  foo.SetHello(42);
+  foo.SetWorld(4.2f);
+  REQUIRE(foo.GetHello() == 42);
+  REQUIRE(foo.GetWorld() == 4.2f);
+}
