b7cbc91ad55b6e14f409504852d44db07c610894
Gustav <sir.gustav.the.coder@gmail.com>
2017-04-12 22:54:02 +0200

    json parsing code in test suite

--- test-all.py
+++ test-all.py
@@ -48,63 +48,75 @@ def main():
     errors = 0
 
     for code in code_examples:
-        print()
-        print('----------------------------------------------------------------')
-        print('--- {}'.format(code.name))
-        print('----------------------------------------------------------------')
-        code_root_folder = os.path.join(build_folder, code.name)
-        remove_folder(code_root_folder)
-        ensure_folder_exist(code_root_folder)
-        with open(os.path.join(code_root_folder, 'CMakeLists.txt'), 'w') as cmake_file:
-            test_cpp_folder = os.path.join(root_folder, 'test-cpp')
-            cmake_file.write('''
-            cmake_minimum_required(VERSION 3.1)
-            project(gaf)
-            set (CMAKE_CXX_STANDARD 11)
-            include_directories(SYSTEM {external}/catch)
-            include_directories(SYSTEM {external}/rapidjson-1.1.0/include)
-            include({root}/gaf.cmake)
-            SET(Gaf_CUSTOM_NAME mygaf)
-            GAF_GENERATE_CPP(GAF_SOURCES GAF_HEADERS {root}/examples/{gaf})
-            include_directories(${{CMAKE_CURRENT_BINARY_DIR}})
-            add_executable(app {cpp} ${{GAF_SOURCES}} ${{GAF_HEADERS}})
-            '''.format(
-                gaf=code.gaf,
-                cpp= os.path.join(test_cpp_folder, code.test),
-                external=os.path.join(test_cpp_folder, 'external'),
-                root=root_folder
-            ))
-        code_build_folder = os.path.join(code_root_folder, 'build')
-        ensure_folder_exist(code_build_folder)
-        cmake_result = ''
-        make_result = ''
-        app_result = ''
-        try:
-            print('running cmake')
-            cmake_result = subprocess.check_output(['cmake', '..'], stderr=subprocess.STDOUT, universal_newlines=True, cwd=code_build_folder)
-            print('running make')
-            make_result = subprocess.check_output(['make'], stderr=subprocess.STDOUT, universal_newlines=True, cwd=code_build_folder)
-            print('running code')
-            code_run_folder = os.path.join(code_build_folder, 'run')
-            ensure_folder_exist(code_run_folder)
-            app_result = subprocess.check_output(['../app'], stderr=subprocess.STDOUT, universal_newlines=True, cwd=code_run_folder)
-        except subprocess.CalledProcessError as exc:
-            eprint('Status: FAIL')
-            eprint('Error code: {}'.format(exc.returncode))
-            eprint(exc.output)
-            if len(cmake_result)>0:
-                print('CMake result:')
-                print(cmake_result)
-                print()
-            if len(make_result)>0:
-                print('Make result:')
-                print(make_result)
-                print()
-            if len(app_result) > 0:
-                print('app result:')
-                print(app_result)
-                print()
-            errors += 1
+        for json_test in [True, False]:
+            codename = code.name
+            if json_test:
+                codename += '_json'
+
+            print()
+            print('----------------------------------------------------------------')
+            print('--- {}'.format(codename))
+            print('----------------------------------------------------------------')
+            code_root_folder = os.path.join(build_folder, code.name)
+
+            remove_folder(code_root_folder)
+            ensure_folder_exist(code_root_folder)
+            with open(os.path.join(code_root_folder, 'cmdlineargs.txt'), 'w') as cmd:
+                if json_test:
+                    cmd.write('--include-json\n')
+
+            with open(os.path.join(code_root_folder, 'CMakeLists.txt'), 'w') as cmake_file:
+                test_cpp_folder = os.path.join(root_folder, 'test-cpp')
+                cmake_file.write('''
+                cmake_minimum_required(VERSION 3.1)
+                project(gaf)
+                set (CMAKE_CXX_STANDARD 11)
+                include_directories(SYSTEM {external}/catch)
+                include_directories(SYSTEM {external}/rapidjson-1.1.0/include)
+                include({root}/gaf.cmake)
+                SET(Gaf_CUSTOM_NAME mygaf)
+                SET(Gaf_CUSTOM_ARGUMENTS_FROM_FILE {coderoot}cmdlineargs.txt)
+                GAF_GENERATE_CPP(GAF_SOURCES GAF_HEADERS {root}/examples/{gaf})
+                include_directories(${{CMAKE_CURRENT_BINARY_DIR}})
+                add_executable(app {cpp} ${{GAF_SOURCES}} ${{GAF_HEADERS}})
+                '''.format(
+                    gaf=code.gaf,
+                    cpp= os.path.join(test_cpp_folder, code.test),
+                    external=os.path.join(test_cpp_folder, 'external'),
+                    root=root_folder,
+                    coderoot=code_root_folder
+                ))
+            code_build_folder = os.path.join(code_root_folder, 'build')
+            ensure_folder_exist(code_build_folder)
+            cmake_result = ''
+            make_result = ''
+            app_result = ''
+            try:
+                print('running cmake')
+                cmake_result = subprocess.check_output(['cmake', '..'], stderr=subprocess.STDOUT, universal_newlines=True, cwd=code_build_folder)
+                print('running make')
+                make_result = subprocess.check_output(['make'], stderr=subprocess.STDOUT, universal_newlines=True, cwd=code_build_folder)
+                print('running code')
+                code_run_folder = os.path.join(code_build_folder, 'run')
+                ensure_folder_exist(code_run_folder)
+                app_result = subprocess.check_output(['../app'], stderr=subprocess.STDOUT, universal_newlines=True, cwd=code_run_folder)
+            except subprocess.CalledProcessError as exc:
+                eprint('Status: FAIL')
+                eprint('Error code: {}'.format(exc.returncode))
+                eprint(exc.output)
+                if len(cmake_result)>0:
+                    print('CMake result:')
+                    print(cmake_result)
+                    print()
+                if len(make_result)>0:
+                    print('Make result:')
+                    print(make_result)
+                    print()
+                if len(app_result) > 0:
+                    print('app result:')
+                    print(app_result)
+                    print()
+                errors += 1
     print()
     print()
     if errors > 0:
