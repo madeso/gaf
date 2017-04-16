71061b8970e082f835a1c91a8b506b600939e5bd
Gustav <sir.gustav.the.coder@gmail.com>
2017-04-16 11:23:23 +0200

    updated array example

--- examples/planned/arrays.c
+++ examples/planned/arrays.c
@@ -4,10 +4,10 @@ struct Foo {
   int32 hello[42]; // arrays are always of constant size
   int32 hello2[LIFE]; // constants can also be used
 
-  int32 world_size;
-  float world[world_size]; // except dynamic arrays, where world_size is the size of the array
+  float world[int32]; // if a type is specified, another variable is created that specifies the size
 
-  int32 world2_items;
-  int32 world2_size;
-  float world2[world2_items, world2_size]; // if a second identifier is specified, the first is the number of items and second value is actual size of the array
+  float world2[int32, int32]; // if a second type is specified, the first is the number of items and second value is actual size of the array
+
+  float world3[int32, 42]; // the second type can also be a constant
+  float world4[int32, LIFE];
 }
