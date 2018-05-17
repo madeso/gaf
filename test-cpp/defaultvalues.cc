6b80b48d79b637e47f38a869b1fa7ca06297da46
Gustav <sir.gustav.the.coder@gmail.com>
2018-05-17 23:26:11 +0200

    fixes

--- test-cpp/defaultvalues.cc
+++ test-cpp/defaultvalues.cc
@@ -10,23 +10,24 @@
 
 TEST_CASE("constructor") {
   const Foo foo;
-  REQUIRE(foo.GetHello() == 12);
-  REQUIRE(foo.GetWorld() == 3.14f);
+  REQUIRE(foo.hello == 12);
+  REQUIRE(foo.world == 3.14f);
 }
 
 TEST_CASE("setter") {
   Foo foo;
-  foo.SetHello(42);
-  foo.SetWorld(4.2f);
-  REQUIRE(foo.GetHello() == 42);
-  REQUIRE(foo.GetWorld() == 4.2f);
+  foo.hello = 42;
+  foo.world = 4.2f;
+  REQUIRE(foo.hello == 42);
+  REQUIRE(foo.world == 4.2f);
 }
 
 TEST_CASE("reset") {
   Foo foo;
-  foo.SetHello(42);
-  foo.SetWorld(4.2f);
-  foo.Reset();
-  REQUIRE(foo.GetHello() == 12);
-  REQUIRE(foo.GetWorld() == 3.14f);
+  foo.hello = 42;
+  foo.world = 4.2f;
+
+  foo = Foo();
+  REQUIRE(foo.hello == 12);
+  REQUIRE(foo.world == 3.14f);
 }
--- test-cpp/package.cc
+++ test-cpp/package.cc
@@ -10,14 +10,14 @@
 
 TEST_CASE("constructor") {
   const test::Foo foo;
-  REQUIRE(foo.GetHello() == 0);
-  REQUIRE(foo.GetWorld() == 0.0f);
+  REQUIRE(foo.hello == 0);
+  REQUIRE(foo.world == 0.0f);
 }
 
 TEST_CASE("setter") {
   test::Foo foo;
-  foo.SetHello(42);
-  foo.SetWorld(4.2f);
-  REQUIRE(foo.GetHello() == 42);
-  REQUIRE(foo.GetWorld() == 4.2f);
+  foo.hello = 42;
+  foo.world = 4.2f;
+  REQUIRE(foo.hello == 42);
+  REQUIRE(foo.world == 4.2f);
 }
