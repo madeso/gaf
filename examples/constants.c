c1bf27587bd9525ee2565a163add91c6f2f0026e
Gustav <sir.gustav.the.coder@gmail.com>
2018-06-01 17:13:46 +0200

    fixed build

--- examples/constants.c
+++ examples/constants.c
@@ -1,9 +1,11 @@
 const int32 INT = 12;
 const float FLOAT = 3.14f;
 const bool BOOL = true;
+const string STR = "dog";
 
 struct Foo {
   int32 hello = INT;
   float world = FLOAT;
   bool b = BOOL;
+  string s = STR;
 }
